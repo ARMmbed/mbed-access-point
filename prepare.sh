8b0df6bbf51279cc0aa7038136fd4c0504829788
Devaraj Ranganna <devaraj.ranganna@arm.com>
2017-08-31 16:59:54 +0100

    [mbed access point] Move target specific configuration to "target" folder
    Signed-off-by: Devaraj Ranganna <devaraj.ranganna@arm.com>

--- prepare.sh
+++ prepare.sh
@@ -7,7 +7,7 @@ if [ "$#" -ne 1 ] ; then
         echo "       ar150 - build an image for GL-AR150"
         exit -1
 elif [ $1 == "rpi2" ] || [ $1 == "rpi3" ] || [ $1 == "ar150" ] ; then
-        break
+        :
 else
         echo "Unrecognised option: " $1
         echo "Usage: prepare.sh rpi2|rpi3|ar150"
@@ -24,9 +24,11 @@ echo "***** Installing feeds *****"
 echo "***** Applying custom kernel configuration *****"
 if [ $1 == "rpi2" ] ; then
         cp target/linux/brcm2708/bcm2709/openwrt_config .config
+        cp target/linux/brcm2708/bcm2709/mcproxy feeds/routing/mcproxy/files/mcproxy.config
 elif [ $1 == "rpi3" ] ; then
         echo "dtoverlay=pi3-miniuart-bt" >> ./target/linux/brcm2708/image/config.txt
         cp target/linux/brcm2708/bcm2710/openwrt_config .config
+        cp target/linux/brcm2708/bcm2710/mcproxy feeds/routing/mcproxy/files/mcproxy.config
 elif [ $1 == "ar150" ] ; then
         cp target/linux/ar71xx/gl-ar150/openwrt_config .config
         cp target/linux/ar71xx/gl-ar150/mcproxy feeds/routing/mcproxy/files/mcproxy.config
